0f87f14a6e0442ee0ab1ab3a4449919717ef9598
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-10-08 17:03:38 -0700

    elaborating on parse_cells function, need to implement

--- collapse.py
+++ collapse.py
@@ -2,16 +2,21 @@
 import sys
 
 def main():
-    for arg in sys.argv:
-        print(arg)
-
+    # validate and parse arguments
     grid_x = int(sys.argv[1])
     grid_y = int(sys.argv[2])
     cell_file = sys.argv[3]
-    # validate and parse arguments
+
+    print("grid dimensions:     %d by %d" % (grid_x, grid_y))
+    print("cell file:           %s" % (cell_file))
+
+    for i in range(grid_y):
+        for j in range(grid_x):
+            print("[]", end='')
+        print('')
 
     # parse cells from file
-    parse_cells(grid_x, grid_y, cell_file)
+    cell_dimensions,cell_structures,cell_connections = parse_cells(grid_x, grid_y, cell_file)
     # generate cell_grid (input x by input y)
 
     # populate grid with cell identifiers based on entropy calculations
@@ -21,9 +26,29 @@ def main():
     # populate print_grid based on cell identifiers in cell_grid
 
 def parse_cells(x, y, cfile):
-    for i in range(y):
-        for j in range(x):
-            print("[]", end='')
-        print('')
+    # open file
+    f = open(cfile, 'r')
+    
+    # get cell dimensions
+
+    # create dictionary cell_structures
+        # identifier:
+            # [
+            #   <cell structure> 
+            # ]
+
+    # create dictionary cell_connections
+        # identifier:
+            # [
+            #   {up     :   [ids]}
+            #   {left   :   [ids]}
+            #   {down   :   [ids]}
+            #   {rigt   :   [ids]}
+            # ]
+
+
+    print("")
+    
+
 
 main()
