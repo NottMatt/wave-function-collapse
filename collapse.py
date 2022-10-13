de0a46b0ad6f5b3ded81ed774e4cf3667b4f8968
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-10-13 16:35:23 -0700

    parsing cell_structures

--- collapse.py
+++ collapse.py
@@ -10,10 +10,6 @@ def main():
     print("grid dimensions:     %d by %d" % (grid_x, grid_y))
     print("cell file:           %s" % (cell_file))
 
-    for i in range(grid_y):
-        for j in range(grid_x):
-            print("[]", end='')
-        print('')
 
     # parse cells from file
     # cell_dimensions,cell_structures,cell_connections = parse_cells(grid_x, grid_y, cell_file)
@@ -32,22 +28,45 @@ def parse_cells(x, y, cfile):
     
     # get cell dimensions
     f_content = f.read()
-    cell_dimensions = f_content.split("{")[0].split("\n")[0].split(" ")[1].split("x")
+    cell_dimensions_str = f_content.split("{")[0].split("\n")[0].split(" ")[1].split("x")
 
-    cell_x = int(cell_dimensions[0])
-    cell_y = int(cell_dimensions[1])
+    cell_x = int(cell_dimensions_str[0])
+    cell_y = int(cell_dimensions_str[1])
 
-    for i in range(y * cell_y):
-        for j in range(x * cell_x):
-            print("[]", end='')
-        print('')
+    cell_dimensions = [cell_x, cell_y]
+
+    # for i in range(y * cell_y):
+    #     for j in range(x * cell_x):
+    #         print("[]", end='')
+    #     print('')
+
     
+    # divide into cells
+
+    cells = f_content.split("{")[1:]
+
+    for i in range(len(cells)):
+        cells[i] = cells[i].split("}")[0].split(";")
+
+    cell_structures = {}
+
+    for i in range(len(cells)):
+        cell_structures[cells[i][0].replace("\n", "")] = cells[i][1].replace("\n", "")
+
+    
+    for i in range(cell_y):
+        for j in range(cell_x):
+            print(cell_structures["six"][j + (i * cell_x)], end='')
+        print("")
 
     # create dictionary cell_structures
         # identifier:
             # [
             #   <cell structure> 
             # ]
+
+
+    cell_connections = {}
 
     # create dictionary cell_connections
         # identifier:
